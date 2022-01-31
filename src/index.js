ad60faf97c4f63fe3319dbcfbba71140e2cb2de5
JimmiJan <96634490+JimmiJan@users.noreply.github.com>
2022-01-31 09:59:37 -0800

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,74 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+ReactDOM.render(
+  <div>
+   <h1>
+                 Name 
+	     </h1>
+
+             <p>
+	 	Muhammad Jamshed
+	     </p>
+
+	     <h1>
+		 Father Name
+	     </h1>
+
+	     <p>
+                Shahid Mehmood
+	     </p>
+
+             <h1>
+                 Section
+	     </h1>
+
+	     <p>
+                C
+	     </p>
+
+	     <h1>
+                 Institute Name
+	     </h1>
+	     <p>
+	        BMJ Institute
+	     </p>
+       <h1>
+                 Course
+	     </h1>
+	     <p>
+	        WEB AD MOBILE APP DEVELOPMENT
+	     </p>
+
+      <h1>
+      It's my pleasure to speak
+       with you. I am (Jamshed Shahid). Basically, 
+       I belong to (Karachi). I have been living in
+        (Karachi) . I 
+        stay here with my family. Besides my parents, 
+        there is a younger brother in my family.  
+        
+      </h1>
+
+       
+  </div>,
+  document.getElementById("root")
+)
